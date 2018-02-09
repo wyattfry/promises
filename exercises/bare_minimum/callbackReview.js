f67e02c649835306f6795941fedaad122ff5eb6d
wyattfry <wyattfry@gmail.com>
2018-02-08 19:55:57 -0800

    Pass getStatusCodeAsync tests

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -5,8 +5,9 @@
 var fs = require('fs');
 var request = require('request');
 
+exports = module.exports = {};
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath, callback) {
+exports.pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
@@ -18,7 +19,7 @@ var pluckFirstLineFromFile = function (filePath, callback) {
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url, callback) {
+exports.getStatusCode = function (url, callback) {
   // TODO
   request(url, (err, resp, body) => {
     callback(err, resp && resp.statusCode);
@@ -26,7 +27,7 @@ var getStatusCode = function (url, callback) {
 };
 
 // Export these functions so we can test them and reuse them in later exercises
-module.exports = {
-  getStatusCode: getStatusCode,
-  pluckFirstLineFromFile: pluckFirstLineFromFile
-};
+// module.exports = {
+//   getStatusCode: getStatusCode,
+//   pluckFirstLineFromFile: pluckFirstLineFromFile
+// };
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -11,11 +11,30 @@ var Promise = require('bluebird');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data.split('\n')[0]);
+      }
+    });
+  });
 };
+
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
   // TODO
+  return new Promise((resolve, reject) => {
+    request(url, (err, resp, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(resp && resp.statusCode);
+      }
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
